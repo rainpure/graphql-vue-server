f633fc4d24b758c903ccccfd675df51cdb4c0af8
cancan <790864964@qq.com>
2017-06-26 18:07:26 +0800

    init

--- data/resolvers.js
+++ data/resolvers.js
@@ -19,18 +19,18 @@ const authors = [
 
 const posts = [
   { id: 1, authorId: 1, title: '枫叶是最帅的', agree: 1 },
-  { id: 2, authorId: 2, title: '不二是最帅的', agree: 2 },
+  { id: 2, authorId: 2, title: '不二是最帅的', agree: 1 },
   { id: 3, authorId: 3, title: '菜瓜是最帅的', agree: 1 },
   { id: 4, authorId: 4, title: '城管是最帅的', agree: 1 },
   { id: 5, authorId: 5, title: '东邪是最帅的', agree: 1 },
   { id: 6, authorId: 6, title: '曼巴是最帅的', agree: 1 },
   { id: 8, authorId: 7, title: '绿箭是最帅的', agree: 1 },
-  { id: 13, authorId: 8, title: '灿灿是最帅的', agree: 10 },
   { id: 7, authorId: 9, title: '糖饼是最帅的', agree: 1 },
   { id: 9, authorId: 10, title: '天吾是最帅的', agree: 1 },
   { id: 10, authorId: 11, title: '小米是最帅的', agree: 1 },
   { id: 11, authorId: 12, title: '熊猫是最帅的', agree: 1 },
   { id: 12, authorId: 13, title: '芝士是最帅的', agree: 1 },
+  { id: 13, authorId: 8, title: '灿灿是最帅的', agree: 1 },
 ];
 
 const resolveFunctions = {
@@ -39,7 +39,7 @@ const resolveFunctions = {
       return posts;
     },
     author(_, { id }) {
-      return find(authors, { id: id });
+      return find(authors, { id });
     },
   },
   Mutation: {
