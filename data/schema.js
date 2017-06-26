57ab78c297392c7b836b772e4580f3650e7e4e6d
cancan <790864964@qq.com>
2017-06-26 22:55:01 +0800

    Add comments

--- data/schema.js
+++ data/schema.js
@@ -5,28 +5,39 @@ import resolvers from './resolvers';
 const schema = `
 # 作者
 type Author {
-  id: Int! # the ! means that every author object _must_ have an id
+  # 作者的id
+  id: Int! 
+  # 作者firstName
   firstName: String
+  # 作者lastName
   lastName: String
-  posts: [Post] # the list of Posts by this author
+  # 作者名下的文章
+  posts: [Post]
 }
 
 # 文章
 type Post {
+  # 文章的id
   id: Int!
+  # 文章的标题
   title: String
+  # 文章的作者
   author: Author
+  # 文章所得投票数
   agree: Int
 }
 
-# the schema allows the following query这里可以写中文
+# 查询 Query
 type Query {
+  # 所有文章
   posts: [Post]
+  # 根据id查询作者
   author(id: Int!): Author
 }
 
-# this schema allows the following mutation这里可以写中文
+# 修改 Mutation
 type Mutation {
+  # 传入postId给对应文章投票
   upvotePost (
     postId: Int!
   ): Post
